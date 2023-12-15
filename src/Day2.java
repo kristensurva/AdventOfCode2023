8545e1aec214f685cf752a7568135bca3f03f38a
Kristen <kristensurva@gmail.com>
2023-12-15 21:43:02 +0200

    Day 2 finished

--- src/Day2.java
+++ src/Day2.java
@@ -1,3 +1,71 @@
-public class Day2 {
+import java.util.Arrays;
+import java.util.List;
 
+public class Day2 {
+    public static int solveTask1(List<String> input) {
+        int redCubesInBag = 12;
+        int greenCubesInBag = 13;
+        int blueCubesInBag = 14;
+        int sum = 0;
+        for (String line : input) {
+            String[] split = line.split(":|;");
+            int gameID = Integer.parseInt(split[0].substring(4).trim());
+            boolean isGameEligible = true;
+            for (String patch : Arrays.copyOfRange(split, 1, split.length)) {
+                if (!isGameEligible) {
+                    break;
+                }
+                for (String string : patch.split(",")) {
+                    //System.out.println(string);
+                    String[] patchSplit = string.trim().split(" ");
+                    int amount = Integer.parseInt(patchSplit[0]);
+                    switch (patchSplit[1]) {
+                        case "red":
+                            if (amount > redCubesInBag) {
+                                isGameEligible = false;
+                            }
+                        case "green":
+                            if (amount > greenCubesInBag) {
+                                isGameEligible = false;
+                            }
+                        case "blue":
+                            if (amount > blueCubesInBag) {
+                                isGameEligible = false;
+                            }
+                    }
+                    if (!isGameEligible) {
+                        break;
+                    }
+                }
+            }
+            if (isGameEligible) {
+                sum += gameID;
+            }
+        }
+        return sum;
+    }
+
+    public static int solveTask2(List<String> input) {
+        int sum = 0;
+        for (String line : input) {
+            int redCubesMaxCurrentGame = 0;
+            int greenCubesMaxCurrentGame = 0;
+            int blueCubesMaxCurrentGame = 0;
+            String[] split = line.split(":|;");
+            for (String patch : Arrays.copyOfRange(split, 1, split.length)) {
+                for (String string : patch.split(",")) {
+                    String[] patchSplit = string.trim().split(" ");
+                    int amount = Integer.parseInt(patchSplit[0]);
+                    switch (patchSplit[1]) {
+                        case "red" -> redCubesMaxCurrentGame = Integer.max(redCubesMaxCurrentGame, amount);
+                        case "green" -> greenCubesMaxCurrentGame = Integer.max(greenCubesMaxCurrentGame, amount);
+                        case "blue" -> blueCubesMaxCurrentGame = Integer.max(blueCubesMaxCurrentGame, amount);
+                    }
+                }
+            }
+            int powerOfSet = redCubesMaxCurrentGame*greenCubesMaxCurrentGame*blueCubesMaxCurrentGame;
+            sum+=powerOfSet;
+        }
+        return sum;
+    }
 }
--- src/Program.java
+++ src/Program.java
@@ -3,16 +3,12 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Program {
     public static void main(String[] args) throws IOException {
         //System.out.println(Day1.solveTask1(readInFile("Inputs/Day1Test.txt")));
         //System.out.println(Day1.solveTask2(readInFile("Inputs/Day1Input.txt")));
-        
-
-
+        //System.out.println(Day2.solveTask1(readInFile("Inputs/Day2Input.txt")));
+        System.out.println(Day2.solveTask2(readInFile("Inputs/Day2Input.txt")));
 
         //System.out.println(Day8.solveTask1(readInFile("Inputs/Day8Input.txt")));
         //System.out.println(Day8.solveTask2(readInFile("Inputs/Day8Input.txt")));
